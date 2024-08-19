97458eab6f7414c2e2a6e3b447e2c3422ed24455
SRINIVAS0034 <srinivasreddy0034@gmail.com>
2024-08-19 14:00:03 +0530

    replace 5th script

--- 05-shell-scripts.sh
+++ 05-shell-scripts.sh
@@ -4,5 +4,8 @@ echo "please enter your username::"
 
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
-echo "username is: $USERNAME"
+echo " please enter your password:"
 
+read -s PASSWORD
+
+echo " username is :$USERNAME, password is : $PASSWORD"
